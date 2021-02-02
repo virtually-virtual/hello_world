dbf4f045b1c223bb5bbeb1660ce02d0e81ce1332
virtually-virtual <60192598+virtually-virtual@users.noreply.github.com>
2021-02-02 04:27:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const os = require('os');
 console.log("Starting...");
 cost handler = (req,res) => { 
  response.writeHead(200);  
- response.end("You've connected " + os.hostname() + "\n")
+ response.end("You've connected to " + os.hostname() + "\n")
 }
 const app = http.createServer(handler)
 app.listen(8080);
